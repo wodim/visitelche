84d24a7857e560a8b98b7596d6cf81fda860eac5
wodim <neikokz@gmail.com>
2020-03-22 16:15:34 +0100

    pescanova update

--- visitelche.py
+++ visitelche.py
@@ -10,15 +10,16 @@ import telepot.aio
 
 MY_NAME = 'visitelchebot'
 MY_COMMAND = '/visitelche'
-MY_MASK = 'images/elche.png'
+MY_COMMAND_P = '/pescanova'
+MY_MASK = 'assets/elche.png'
 
 FFMPEG_CMD = ('ffmpeg -hide_banner '
               # first input: base video
               '-i \'{source}\' '
               # second input: images to overlay
-              '-loop 1 -start_number 1 -start_number_range 8 -framerate 30 -i images/elche_tiktok_%02d.png '
+              '-loop 1 -start_number 1 -start_number_range 8 -framerate 30 -i assets/elche_tiktok_%02d.png '
               # third input: subtitle
-              '-loop 1 -framerate 30 -i images/elche_tiktok_sub.png '
+              '-loop 1 -framerate 30 -i assets/elche_tiktok_sub.png '
               # filter
               '-filter_complex "'
               '[0:v]fps=fps=30[base];'  # convert source to 30 fps
@@ -29,6 +30,15 @@ FFMPEG_CMD = ('ffmpeg -hide_banner '
               '[base][logo]overlay=0:0:eof_action=endall[v]'  # put logo over video
               # output options
               '" -map [v] -map 0:a? -c:a copy -y -preset ultrafast -crf 27 -threads 4 '
+              '\'{dest}\'')
+
+FFMPEG_CMP = ('ffmpeg -hide_banner '
+              # first input: base video
+              '-i \'{source}\' '
+              # second input: base audio
+              '-i assets/pescanova.aac '
+              # output options
+              ' -map 0:0 -map 1:0 -c:a copy -c:v copy -shortest -y '
               '\'{dest}\'')
 
 
@@ -57,28 +67,31 @@ class TelegramBot(telepot.aio.Bot):
             if self.is_media_message(message):
                 await self.process(message)
             else:
-                if self.check_mention(message) and chat_id in self.last_msg_w_media:
-                    await self.process(self.last_msg_w_media[chat_id])
+                mention = self.check_mention(message)
+                if mention and chat_id in self.last_msg_w_media:
+                    await self.process(self.last_msg_w_media[chat_id], mention)
                 else:
                     await self.send_message(message, caption='e')
         elif chat_type in self.PUBLIC_CHATS:
-            if self.check_mention(message):
+            mention = self.check_mention(message)
+            if mention:
                 if 'reply_to_message' in message:
                     message = message['reply_to_message']
                 if self.is_media_message(message):
-                    await self.process(message)
+                    await self.process(message, mention)
                 # don't reply with something from last_msg_w_media if this is a reply message
                 elif chat_id in self.last_msg_w_media and 'reply_to_message' not in message:
-                    await self.process(self.last_msg_w_media[chat_id])
+                    await self.process(self.last_msg_w_media[chat_id], mention)
                 else:
                     await self.send_message(message, caption='e')
 
-
-    async def process(self, message):
-        if 'photo' in message:
+    async def process(self, message, type_=None):
+        if 'photo' in message and type_ is None:
             await self.process_photo(message)
         elif 'video' in message or 'animation' in message:
-            await self.process_video(message)
+            await self.process_video(message, type_)
+        else:
+            await self.send_message(message, caption='mis kojones')
 
     @staticmethod
     def is_media_message(message):
@@ -105,7 +118,7 @@ class TelegramBot(telepot.aio.Bot):
             await self.send_message(message, caption='no he podido enviar la foto tuneada :(')
         print('Sent')
 
-    async def process_video(self, message):
+    async def process_video(self, message, type_):
         if self.busy:
             await self.send_message(message, caption='estoy haciendo cositas, inténtalo luego')
         if 'video' in message:
@@ -128,7 +141,10 @@ class TelegramBot(telepot.aio.Bot):
                 return
         print('Downloaded. Composing')
         new_filename = 'tmp/%s_.mp4' % file_id
-        cmd = FFMPEG_CMD.format(source=file_dest, dest=new_filename)
+        if type_ == MY_COMMAND_P:
+            cmd = FFMPEG_CMP.format(source=file_dest, dest=new_filename)
+        else:
+            cmd = FFMPEG_CMD.format(source=file_dest, dest=new_filename)
         print(cmd)
         self.busy = True
         subprocess.call(cmd, shell=True)
@@ -188,8 +204,11 @@ class TelegramBot(telepot.aio.Bot):
             text = msg['caption']
         else:
             return False
-        return ('@' + MY_NAME.lower() in text.lower() or
-                text.lower().startswith(MY_COMMAND))
+        if text.lower().startswith(MY_COMMAND_P):
+            return MY_COMMAND_P
+        if ('@' + MY_NAME.lower() in text.lower() or
+                text.lower().startswith(MY_COMMAND)):
+            return MY_COMMAND
 
     @staticmethod
     def ellipsis(text, max_):
