193729b5f9854dd40b552dea17e78d36bca680e5
wodim <wodim@vortigaunt.net>
2021-08-13 14:01:04 +0200

    megaalvise update

--- visitelche.py
+++ visitelche.py
@@ -15,43 +15,59 @@ CMD_VISITELCHE = '/visitelche'
 CMD_PESCANOVA = '/pescanova'
 CMD_BULO = '/bulo'
 CMD_ALVISE = '/alvise'
+CMD_MEGAALVISE = '/megaalvise'
 
 IMAGE_CMDS = (CMD_VISITELCHE, CMD_BULO, CMD_ALVISE)
 VIDEO_CMDS = (CMD_VISITELCHE, CMD_PESCANOVA)
+ITOV_CMDS = (CMD_MEGAALVISE,)
 
 MASKS = {
     CMD_VISITELCHE: ('assets/elche.png',),
     CMD_BULO: ('assets/bulo1.png', 'assets/bulo2.png', 'assets/bulo3.png',),
     CMD_ALVISE: ('assets/alvise.png',),
+    CMD_MEGAALVISE: ('assets/alvise_mini.png',),
 }
 
-FFMPEG_CMD = ('ffmpeg -hide_banner '
-              # first input: base video
-              '-i \'{source}\' '
-              # second input: images to overlay
-              '-loop 1 -start_number 1 -start_number_range 8 -framerate 30 -i assets/elche_tiktok_%02d.png '
-              # third input: subtitle
-              '-loop 1 -framerate 30 -i assets/elche_tiktok_sub.png '
-              # filter
-              '-filter_complex "'
-              '[0:v]fps=fps=30[base];'  # convert source to 30 fps
-              '[base]scale=w=-2:h=600[base];'  # resize source to 600 px min
-              '[1:v][base]scale2ref=oh*mdar:h=ih/8[logo1][base];'  # resize moving logo
-              '[2:v][base]scale2ref=oh*mdar:h=ih/4[logo2][base];'  # resize subtitle
-              '[logo2][logo1]overlay=0:0:eof_action=endall[logo];'  # merge both logos
-              '[base][logo]overlay=0:0:eof_action=endall[v]'  # put logo over video
-              # output options
-              '" -map [v] -map 0:a? -c:a copy -y -preset ultrafast -crf 27 -threads 4 '
-              '\'{dest}\'')
+FFMPEG_CMD_VISITELCHE = (
+    'ffmpeg -hide_banner '
+    # first input: base video
+    '-i \'{source}\' '
+    # second input: images to overlay
+    '-loop 1 -start_number 1 -start_number_range 8 -framerate 30 -i assets/elche_tiktok_%02d.png '
+    # third input: subtitle
+    '-loop 1 -framerate 30 -i assets/elche_tiktok_sub.png '
+    # filter
+    '-filter_complex "'
+    '[0:v]fps=fps=30[base];'  # convert source to 30 fps
+    '[base]scale=w=-2:h=600[base];'  # resize source to 600 px min
+    '[1:v][base]scale2ref=oh*mdar:h=ih/8[logo1][base];'  # resize moving logo
+    '[2:v][base]scale2ref=oh*mdar:h=ih/4[logo2][base];'  # resize subtitle
+    '[logo2][logo1]overlay=0:0:eof_action=endall[logo];'  # merge both logos
+    '[base][logo]overlay=0:0:eof_action=endall[v]'  # put logo over video
+    # output options
+    '" -map [v] -map 0:a? -c:a copy -y -preset ultrafast -crf 27 -threads 4 '
+    '\'{dest}\''
+)
 
-FFMPEG_CMP = ('ffmpeg -hide_banner '
-              # first input: base video
-              '-i \'{source}\' '
-              # second input: base audio
-              '-i assets/pescanova.aac '
-              # output options
-              ' -map 0:0 -map 1:0 -c:a copy -c:v copy -shortest -y '
-              '\'{dest}\'')
+FFMPEG_CMD_PESCANOVA = (
+    'ffmpeg -hide_banner '
+    # first input: base video
+    '-i \'{source}\' '
+    # second input: base audio
+    '-i assets/pescanova.aac '
+    # output options
+    ' -map 0:0 -map 1:0 -c:a copy -c:v copy -shortest -y '
+    '\'{dest}\''
+)
+
+FFMPEG_CMD_MEGAALVISE = (
+    'ffmpeg -hide_banner -framerate 15 '
+    # first input: base video
+    '-pattern_type glob -i \'{source}\' '
+    # output options
+    ' -r 15 -shortest -y '
+    '\'{dest}\''
+)
 
 
 class TelegramBot(telepot.aio.Bot):
@@ -103,6 +119,8 @@ class TelegramBot(telepot.aio.Bot):
     async def process(self, message, type_=None):
         if 'photo' in message and (type_ in IMAGE_CMDS or type_ is None):
             await self.process_image(message, type_)
+        elif 'photo' in message and (type_ in ITOV_CMDS):
+            await self.process_itov(message, type_)
         elif ('video' in message or 'animation' in message) and type_ in VIDEO_CMDS:
             await self.process_video(message, type_)
         else:
@@ -123,11 +141,10 @@ class TelegramBot(telepot.aio.Bot):
     @staticmethod
     def get_possible_commands(message):
         if TelegramBot.is_photo_message(message):
-            return IMAGE_CMDS
+            return IMAGE_CMDS + ITOV_CMDS
         elif TelegramBot.is_video_message(message):
             return VIDEO_CMDS
-        else:
-            return ()
+        return ()
 
     async def process_image(self, message, type_):
         file_id = message['photo'][-1]['file_id']
@@ -138,6 +155,7 @@ class TelegramBot(telepot.aio.Bot):
                 await self.download_file(file_id, file_dest)
             except:
                 await self.send_message(message, caption='no me he podido bajar la foto :(')
+                return
         print('Downloaded. Composing')
         new_filename = compose(file_dest, type_)
         print('Composed. Sending')
@@ -148,6 +166,46 @@ class TelegramBot(telepot.aio.Bot):
                                                                      type_='photo', filename=new_filename)
         except:
             await self.send_message(message, caption='no he podido enviar la foto tuneada :(')
+        print('Sent')
+
+    async def process_itov(self, message, type_):
+        file_id = message['photo'][-1]['file_id']
+        file_dest = 'tmp/%s.jpg' % file_id
+        if not os.path.exists(file_dest):
+            print('Downloading')
+            try:
+                await self.download_file(file_id, file_dest)
+            except:
+                await self.send_message(message, caption='no me he podido bajar la foto :(')
+                return
+        wait_msg = await self.send_message(message, caption=('vale, recibido. no me atosigues porque soy '
+                                                             'un pobre procesador arm de 3€ al mes'))
+        print('Downloaded. Composing')
+        for i in range(100):
+            previous = '%s_%03d.jpg' % (file_dest, i - 1) if i != 0 else file_dest
+            next_ = '%s_%03d.jpg' % (file_dest, i)
+            new_filename = compose(previous, type_, next_)
+        new_filename = 'tmp/%s_.mp4' % file_id
+        cmd = FFMPEG_CMD_MEGAALVISE.format(source='%s_*.jpg' % file_dest,
+                                           dest=new_filename)
+        print(cmd)
+        subprocess.call(cmd, shell=True)
+        if not os.path.exists(new_filename):
+            print('ffmpeg did not output anything')
+            await self.send_message(message, caption='no he podido crear el vídeo tuneado :(')
+            return
+        print('Composed. Sending')
+        try:
+            _, _, chat_id, _, msg_id = telepot.glance(message, long=True)
+            await self.sendChatAction(chat_id, 'upload_video')
+            self.last_msg_w_media[chat_id] = await self.send_message(message, quote_msg_id=msg_id,
+                                                                     type_='file', filename=new_filename)
+        except Exception as exc:
+            await self.send_message(message, caption='no he podido enviar el vídeo tuneado :(')
+            print('Failed to send video. Error was: ' + str(exc))
+            return
+        finally:
+            await self.deleteMessage(telepot.message_identifier(wait_msg))
         print('Sent')
 
     async def process_video(self, message, type_):
@@ -173,9 +231,9 @@ class TelegramBot(telepot.aio.Bot):
         print('Downloaded. Composing')
         new_filename = 'tmp/%s_.mp4' % file_id
         if type_ == CMD_PESCANOVA:
-            cmd = FFMPEG_CMP.format(source=file_dest, dest=new_filename)
+            cmd = FFMPEG_CMD_PESCANOVA.format(source=file_dest, dest=new_filename)
         else:
-            cmd = FFMPEG_CMD.format(source=file_dest, dest=new_filename)
+            cmd = FFMPEG_CMD_VISITELCHE.format(source=file_dest, dest=new_filename)
         print(cmd)
         subprocess.call(cmd, shell=True)
         if not os.path.exists(new_filename):
@@ -235,7 +293,7 @@ class TelegramBot(telepot.aio.Bot):
             return False
         if text.lower() == '@' + MY_NAME.lower():
             return CMD_VISITELCHE
-        for cmd in IMAGE_CMDS + VIDEO_CMDS:
+        for cmd in IMAGE_CMDS + VIDEO_CMDS + ITOV_CMDS:
             if (text.lower() == cmd or
                     text.lower().startswith(cmd + '@' + MY_NAME)):
                 return cmd
@@ -245,11 +303,11 @@ class TelegramBot(telepot.aio.Bot):
         return text[:max_ - 3] + '...' if len(text) > max_ else text
 
 
-def compose(filename, type_):
+def compose(filename, type_, new_filename=None):
     def clamp(number, minn, maxn):
         return max(min(maxn, number), minn)
 
-    if type_ not in IMAGE_CMDS:
+    if type_ not in IMAGE_CMDS + ITOV_CMDS:
         raise ValueError('incorrect type for compose %s' % type_)
 
     with Image(filename=filename) as original:
@@ -270,7 +328,7 @@ def compose(filename, type_):
     elif type_ == CMD_BULO:
         mask_img.transform(resize='%dx%d' % (bg_img.width, bg_img.height))
         bg_img.composite(mask_img, gravity='center')
-    elif type_ == CMD_ALVISE:
+    elif type_ in (CMD_ALVISE, CMD_MEGAALVISE):
         mask_img.rotate(random.uniform(-35, -5))
         scaling_factor = random.uniform(.5, .7)
         mask_w, mask_h = (bg_img.width * scaling_factor,
@@ -281,7 +339,8 @@ def compose(filename, type_):
         bg_img.composite(mask_img, left=offset_left, top=offset_top)
 
     bg_img.compression_quality = 100
-    new_filename = 'tmp/masked_%s.jpg' % (filename.replace('/', '_'))
+    if not new_filename:
+        new_filename = 'tmp/masked_%s.jpg' % (filename.replace('/', '_'))
     bg_img.save(filename=new_filename)
     return new_filename
 
